bec49f56353ae6ba4c9544c411f2ee5002997506
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-02-08 14:38:28 +0800

    Update script.py

--- Trailhead_Trails/script.py
+++ Trailhead_Trails/script.py
@@ -8,24 +8,19 @@ req = requests.get(URL)
 soup = BeautifulSoup(req.content, 'html.parser')
 
 columns = ['Trail Name', 'Description', 'URL']
-print(columns)
 df = pd.DataFrame()
 
 container_class = 'tiles-filterable tiles-container'
 output_container = soup.find('div', {'class':container_class})
 
 articles = output_container.find_all('article')
-i = 0
-
 for article in articles:
     trialheadName = article.h3.a['title']
     page_url = BASE_URL + article.h3.a['href']
     description = article.find('div', {'class':'tile-description'}).text
-    i +=1
-    print(trialheadName)
     df = df.append({'Trial Name': trialheadName,
                     'Description': description,
                     'URL': page_url}, ignore_index=True)
 
-df.to_csv('Trail.csv', index=False)
+df.to_csv('Trail Output.csv', index=False)
 print('Export complete')
